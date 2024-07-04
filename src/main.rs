dab501ae841b91fa54ca35549442f5de82aa16e8
Harumi Kiyama <harumi@Harumis-MacBook-Air.local>
2024-07-04 16:05:50 +0800

    Add a tokenizer to allow space characters between tokens

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::VecDeque;
 use std::env;
+use std::mem;
+use std::ops::Index;
 
 #[derive(Debug)]
 struct MyError {
@@ -17,6 +20,90 @@ impl std::error::Error for MyError {
     }
 }
 
+#[derive(Debug, PartialEq)]
+enum Token {
+    TkReserved { raw: String },      // Keywords or punctuators
+    TkNum { raw: String, val: i32 }, // Integer literals
+    TkEof,                           // End-of-file markers
+}
+
+#[derive(Debug)]
+struct TokenQueue(VecDeque<Token>);
+
+impl Index<usize> for TokenQueue {
+    type Output = Token;
+    fn index<'a>(&'a self, i: usize) -> &'a Token {
+        &self.0[i]
+    }
+}
+
+impl TokenQueue {
+    fn except_num(&mut self) -> Result<i32, MyError> {
+        match self.0.pop_front() {
+            Some(Token::TkNum { val, .. }) => {Ok(val)}
+            _ => Err(MyError {
+                info: "wrong token need TkNum".to_string(),
+            })?,
+        }
+    }
+
+    fn comsume(&mut self, op: char) -> Result<bool, MyError> {
+        match self.0.front() {
+            Some(Token::TkReserved { raw }) => {
+                if raw == &op.to_string() {
+                    self.0.pop_front();
+                    Ok(true)
+                } else {
+                    Ok(false)
+                }
+            }
+            _ => Err(MyError {
+                info: "not a TkReserved token".to_string(),
+            }),
+        }
+    }
+
+    fn tokenizer(s: &str) -> Result<Self, MyError> {
+        let mut tokens: VecDeque<Token> = VecDeque::new();
+        let mut chars = s.chars();
+        let mut num = String::new();
+        while let Some(c) = chars.next() {
+            match c {
+                ' ' => {}
+                '0'..='9' => {
+                    num.push(c);
+                }
+                '+' | '-' => {
+                    tokens.push_back(Token::TkNum {
+                        val: num.parse::<i32>().map_err(|e| MyError {
+                            info: e.to_string(),
+                        })?,
+                        raw: mem::take(&mut num),
+                    });
+                    tokens.push_back(Token::TkReserved { raw: c.to_string() });
+                }
+                _ => {
+                    Err(MyError {
+                        info: format!("invalid char {}", c),
+                    })?;
+                }
+            }
+        }
+
+        if num.len() > 0 {
+            tokens.push_back(Token::TkNum {
+                val: num.parse::<i32>().map_err(|e| MyError {
+                    info: e.to_string(),
+                })?,
+                raw: num,
+            });
+        }
+
+        tokens.push_back(Token::TkEof);
+        Ok(Self(tokens))
+    }
+}
+
 fn main() -> Result<(), MyError> {
     let args: Vec<String> = env::args().skip(1).collect();
     if args.len() != 1 {
@@ -24,45 +111,18 @@ fn main() -> Result<(), MyError> {
             info: format!("args error {:?}", args),
         })?;
     }
+
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("  main:");
     let arg = &args[0];
-    let mut nums: Vec<String> = vec![];
-    let mut chars = arg.chars();
-    let mut num = String::new();
-    while let Some(c) = chars.next() {
-        match c {
-            '0'..='9' => {
-                num.push(c);
-            }
-            '+' | '-' => {
-                nums.push(num.clone());
-                num.clear();
-                nums.push(c.to_string());
-            }
-            _ => {
-                Err(MyError {
-                    info: format!("invalid char {}", c),
-                })?;
-            }
-        }
-    }
-    if num.len() > 0 {
-        nums.push(num);
-    }
-    println!("  mov rax, {}", nums[0]);
-    for num in nums.iter().skip(1) {
-        match num.as_str() {
-            "+" => {
-                print!("  add rax, ");
-            }
-            "-" => {
-                print!("  sub rax, ");
-            }
-            _ => {
-                println!("{}", num);
-            }
+    let mut tokens = TokenQueue::tokenizer(arg)?;
+    println!("  mov rax, {}", tokens.except_num()?);
+    while tokens[0] != Token::TkEof {
+        if tokens.comsume('+')? {
+            println!("  add rax, {}", tokens.except_num()?);
+        } else if tokens.comsume('-')? {
+            println!("  sub rax, {}", tokens.except_num()?);
         }
     }
     println!("  ret");
